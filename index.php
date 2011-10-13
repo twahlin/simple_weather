ffb6f4f750da74bef9891670f5ae0411b36a0a2c
Tom Wahlin <tom.wahlin@space150.com>
2011-10-13 17:56:52 -0500

    Initial push with public boilerplate

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title></title>
+
+    <link rel="stylesheet" href="public/styles/style.css" type="text/css" />
+    <link rel="Shortcut Icon" href="public/images/favicon.ico" type="image/x-icon" />
+
+    <script src="public/scripts/jquery-1.4.4.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="public/scripts/scripts.js" type="text/javascript" charset="utf-8"></script>	
+</head>
+
+<body>
+
+&copy; <? echo date("Y");?> 
+
+</body>
+</html>
